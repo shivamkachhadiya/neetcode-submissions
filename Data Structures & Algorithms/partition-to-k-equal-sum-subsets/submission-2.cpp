5f5a013533543db61a262ecfdb9d68bc9a015bb9
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-05-30 20:28:16 +0530

    Add: partition-to-k-equal-sum-subsets - submission-2

---
+++ Data Structures & Algorithms/partition-to-k-equal-sum-subsets/submission-2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool solve(vector<int>&nums,int k,int target,int i,int sum){
+        if(i==nums.size()){
+            if(sum==target){
+                return true;
+            }
+            return false;
+        }
+
+        //pick
+        bool pick=solve(nums,k,target,i+1,sum+nums[i]);
+        //non pick
+        bool nonPick=solve(nums,k,target,i+1,sum);
+
+        return pick||nonPick;
+    }
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int n=nums.size();
+        int sum=0;
+        for(auto &i:nums){
+            sum+=i;
+        }
+
+        if(sum%k==0){
+            int target=sum/k;
+            return solve(nums,k,sum,0,0);
+        }else{
+            return false;
+        }
+    }
+};
