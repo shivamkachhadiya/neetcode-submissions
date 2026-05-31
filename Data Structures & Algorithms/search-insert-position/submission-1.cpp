5dbcb7b52964331fe4301657d6f96b3b168446fe
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-05-31 19:23:07 +0530

    Add: search-insert-position - submission-1

---
+++ Data Structures & Algorithms/search-insert-position/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& arr, int target) {
+        int start=0;
+        int n=arr.size();
+        int end=n-1;
+        while(start<end){
+            int mid=(start+end)/2;
+            if(arr[mid]<target){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return end;
+    }
+};
